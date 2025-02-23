e0a55d59708c8191d7919d332c755d8cbfd43b5f
Eric Moss <1670755+dohnutt@users.noreply.github.com>
2025-02-22 21:13:58 -0500

    Add deepMerge function

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 /**
- * Utility functions, for example the slugify function.
+ * Utility functions
  */
 
 function slugify(str) {
@@ -25,9 +25,34 @@ function jsonForFile(data) {
 		data
 	}, null, 2);
 }
+
+function isObject(item) {
+	return item !== null && typeof item === 'object' && !Array.isArray(item);
+}
+
+function deepMerge(target, ...sources) {
+	if (!sources.length) return target;
+	const source = sources.shift();
+
+	if (isObject(target) && isObject(source)) {
+		for (const key in source) {
+			if (Object.prototype.hasOwnProperty.call(source, key)) {
+				const sourceValue = source[key];
+				if (isObject(sourceValue) && isObject(target[key])) {
+					target[key] = deepMerge(target[key], sourceValue);
+				} else {
+					target[key] = sourceValue;
+				}
+			}
+		}
+	}
+
+	return deepMerge(target, ...sources);
+}
   
 module.exports = {
 	slugify,
 	normalize,
-	jsonForFile
+	jsonForFile,
+	deepMerge
 };
